90afe51057e4b310ab731938564b0673e382509f
abdullahrashid1638 <abdullahrashid1638@gmail.com>
2025-04-10 23:57:16 +0500

    first commit

---
+++ backend.js
@@ -0,0 +1,108 @@
+const express = require('express')
+const app = express()
+
+// socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
+
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+const backEndPlayers = {}
+const backEndProjectiles = {}
+let projectileId = 0
+
+const SPEED = 10
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  backEndPlayers[socket.id] = {
+    x: 500 * Math.random(),
+    y: 500 * Math.random(),
+    color: `hsl(${Math.round(Math.random() * 360)}, 100%, 50%)`,
+    sequenceNumber: 0
+  }
+
+  io.emit('updatePlayers', backEndPlayers)
+
+  socket.on('initCanvas', ({ w, h }) => {
+    backEndPlayers[socket.id].canvas = { w, h }
+  })
+
+  socket.on('shoot', ({ x, y, angle }) => {
+    projectileId++
+
+    const velocity = {
+      x: Math.cos(angle) * 5,
+      y: Math.sin(angle) * 5
+    }
+
+    backEndProjectiles[projectileId] = { 
+      x,
+      y, 
+      velocity, 
+      playerId: socket.id 
+    }
+
+    console.log(backEndProjectiles)
+  })
+
+  socket.on('disconnect', (reason) => {
+    console.log(reason)
+    delete backEndPlayers[socket.id]
+    io.emit('updatePlayers', backEndPlayers)
+  })  
+
+  socket.on('keydown', ({ keycode, sequenceNumber }) => {
+    backEndPlayers[socket.id].sequenceNumber = sequenceNumber
+    switch (keycode) {
+      case 'KeyW':
+        backEndPlayers[socket.id].y -= SPEED
+        break;
+      case 'KeyA':
+        backEndPlayers[socket.id].x -= SPEED
+        break;
+      case 'KeyS':
+        backEndPlayers[socket.id].y += SPEED
+        break;
+      case 'KeyD':
+        backEndPlayers[socket.id].x += SPEED
+        break;
+    }
+  })
+
+  // console.log(backEndPlayers)
+})
+
+// backend ticker
+setInterval(() => {
+
+  // update projectiles' positions here
+  for (const id in backEndProjectiles) {
+    backEndProjectiles[id].x += backEndProjectiles[id].velocity.x
+    backEndProjectiles[id].y += backEndProjectiles[id].velocity.y
+
+    const PROJECTILE_RADIUS = 5
+    if (
+      backEndProjectiles[id].x - PROJECTILE_RADIUS >= 
+      backEndPlayers[backEndProjectiles[id].playerId]?.canvas?.w
+    ) {
+      delete backEndProjectiles[id]
+    }
+    console.log(backEndProjectiles)
+  }
+
+  io.emit('updateProjectiles', backEndProjectiles)
+  io.emit('updatePlayers', backEndPlayers)
+}, 15)
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ public/js/frontend.js
@@ -0,0 +1,198 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+const socket = io()
+
+const scoreEl = document.querySelector('#scoreEl')
+
+const devicePixelRatio = window.devicePixelRatio || 1
+
+canvas.width = innerWidth * devicePixelRatio
+canvas.height = innerHeight * devicePixelRatio
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+const frontEndPlayers = {}
+const frontEndProjectiles = {}
+
+socket.on('connect', () => {
+  socket.emit('initCanvas', { w: canvas.width, h: canvas.height })
+})
+
+socket.on('updateProjectiles', (backEndProjectiles) => {
+  for (const id in backEndProjectiles) {
+    const backEndProjectile = backEndProjectiles[id]
+
+    if (!frontEndProjectiles[id]) {
+      frontEndProjectiles[id] = new Projectile({ 
+        x: backEndProjectile.x, 
+        y: backEndProjectile.y, 
+        radius: 5, 
+        color: frontEndPlayers[backEndProjectile.playerId]?.color, 
+        velocity: backEndProjectile.velocity
+      })
+    } else {
+      frontEndProjectiles[id].x += backEndProjectiles[id].velocity.x
+      frontEndProjectiles[id].y += backEndProjectiles[id].velocity.y
+    }
+  }
+})
+
+socket.on('updatePlayers', (backEndPlayers) => {
+  for (const id in backEndPlayers) {
+    const backEndPlayer = backEndPlayers[id]
+
+    if (!frontEndPlayers[id]) {
+      frontEndPlayers[id] = new Player({ 
+        x: backEndPlayer.x, 
+        y: backEndPlayer.y, 
+        radius: 10, 
+        color: backEndPlayer.color
+      })
+    } else {
+      // moving your own player thrpugh server reconciliation
+      if (id === socket.id) {
+        frontEndPlayers[id].x = backEndPlayer.x
+        frontEndPlayers[id].y = backEndPlayer.y
+        
+        const lastBackendInputIndex = playerInputs.findIndex(input => (
+          backEndPlayer.sequenceNumber === input.sequenceNumber
+        ))
+        
+        if (lastBackendInputIndex > -1) playerInputs.splice(0, lastBackendInputIndex + 1)
+          
+          playerInputs.forEach(input => {
+            frontEndPlayers[id].x += input.dx
+            frontEndPlayers[id].y += input.dy
+          })
+        } else {
+          // for all other players
+          frontEndPlayers[id].x = backEndPlayer.x
+          frontEndPlayers[id].y = backEndPlayer.y
+
+          gsap.to(frontEndPlayers[id].x, {
+            x: backEndPlayer.x,
+            y: backEndPlayer.y,
+            duration: 0.015,
+            ease: 'linear'
+          })
+        }
+      } 
+  }
+
+  for (const id in frontEndPlayers) {
+    if (!backEndPlayers[id]) {
+      delete frontEndPlayers[id]
+    }
+  }
+})
+
+let animationId
+function animate() {
+  animationId = requestAnimationFrame(animate)
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+
+  for (const id in frontEndPlayers) {
+    const player = frontEndPlayers[id]
+    player.draw()
+  }  
+
+  for (const id in frontEndProjectiles) {
+    const frontEndProjectile = frontEndProjectiles[id]
+    frontEndProjectile.draw()
+  }
+}
+
+animate()
+
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
+
+const SPEED = 10
+const playerInputs = []
+let sequenceNumber = 0
+
+setInterval(() => {
+  if (keys.w.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: 0, dy: -SPEED })
+    frontEndPlayers[socket.id].y -= SPEED
+    socket.emit('keydown', { keycode: 'KeyW', sequenceNumber })
+  }
+
+  if (keys.a.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: -SPEED, dy: 0 })
+    frontEndPlayers[socket.id].x -= SPEED
+    socket.emit('keydown', { keycode: 'KeyA', sequenceNumber })
+  }
+
+  if (keys.s.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: 0, dy: SPEED })
+    frontEndPlayers[socket.id].y += SPEED
+    socket.emit('keydown', { keycode: 'KeyS', sequenceNumber })
+  }
+
+  if (keys.d.pressed) {
+    sequenceNumber++
+    playerInputs.push({ sequenceNumber, dx: SPEED, dy: 0 })
+    frontEndPlayers[socket.id].x += SPEED
+    socket.emit('keydown', { keycode: 'KeyD', sequenceNumber })
+  }
+
+  // console.log(playerInputs)
+}, 15)
+
+window.addEventListener('keydown', (event) => {
+  if (!frontEndPlayers[socket.id]) return
+
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = true
+      break
+    case 'KeyA':
+      keys.a.pressed = true
+      break
+    case 'KeyS':
+      keys.s.pressed = true
+      break
+    case 'KeyD':
+      keys.d.pressed = true
+      break
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  if (!frontEndPlayers[socket.id]) return
+
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = false
+      break
+    case 'KeyA':
+      keys.a.pressed = false
+      break
+    case 'KeyS':
+      keys.s.pressed = false
+      break
+    case 'KeyD':
+      keys.d.pressed = false
+      break
+  }
+})
+
